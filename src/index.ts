adcbf52398fc836c7e2eb5db4fabe3c1f2c644fc
cdnguy425 <warrecha@usd260.com>
2025-11-12 14:32:42 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-export default {
-	async fetch(request, env) {
-		const inputs = {
-			prompt: "cyberpunk cat",
-		};
+// src/index.ts
+var index_default = {
+  async fetch(request, env) {
+    const url = new URL(request.url)
+    const inputs = {
+      prompt: url.searchParams.get('prompt')
 
-		const response = await env.AI.run(
-			"@cf/stabilityai/stable-diffusion-xl-base-1.0",
-			inputs,
-		);
-
-		return new Response(response, {
-			headers: {
-				"content-type": "image/png",
-			},
-		});
-	},
-} satisfies ExportedHandler<Env>;
+    };
+    const response = await env.AI.run(
+      "@cf/stabilityai/stable-diffusion-xl-base-1.0",
+      inputs
+    );
+    return new Response(response, {
+      headers: {
+        "content-type": "image/png"
+      }
+    });
+  }
+};
+export {
+  index_default as default
+};
+//# sourceMappingURL=index.js.map
